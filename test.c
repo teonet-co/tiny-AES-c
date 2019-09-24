e9bc545d5ffc732750f99276f226dc83e9647a68
Dmitry Chernikov <d_chernikov@wargaming.net>
2019-09-24 21:45:12 +0300

    Disable clang-format

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+// clang-format off
 
 // Enable ECB, CTR and CBC mode. Note this can be done before including aes.h or at compile-time.
 // E.g. with GCC by using the -D flag: gcc -c aes.c -DCBC=0 -DCTR=1 -DECB=1
@@ -314,3 +315,4 @@ static int test_decrypt_ecb(void)
 }
 
 
+// clang-format on
